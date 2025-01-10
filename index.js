8dd121675c5c45e1c912993a94bd7211e06c4f4a
Michelle Di Guglielmo <mfdigug@gmail.com>
2025-01-10 13:22:37 +1100

    done with javascript functions continued

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(workActivity = 'go to the office') {
+    return `This Monday, I will ${workActivity}.`;
+}
+
+function wrapAdjective(visualFlair = '*'){
+    return function (encouragingPrompt = 'special') {
+         return `You are ${visualFlair}${encouragingPrompt}${visualFlair}!`
+     }; 
+ };
